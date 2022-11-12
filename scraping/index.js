7f57eda4865bc578638c5a5891493e3d945779e9
Sebastian Waisbrot <seppo0010@gmail.com>
2022-11-12 16:00:19 -0300

    Initial commit

---
+++ scraping/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    await page.goto('https://www.corteidh.or.cr/casos_sentencias.cfm');
+    await page.waitForSelector('.search-result a');
+
+    await page._client().send("Page.setDownloadBehavior", {
+      behavior: "allow",
+      downloadPath: './data',
+    });
+
+    await page.exposeFunction('fileExists', f => fs.existsSync(f));
+    const resultsSelector = '.search-result a'
+    const links = (await page.evaluate(resultsSelector => {
+      return [...document.querySelectorAll(resultsSelector)].map(async (anchor) => {
+        const segments = anchor.href.split('/');
+        const filename = segments[segments.length - 1];
+        if (
+            !(await window.fileExists(`data/${filename}`)) &&
+            anchor.href.startsWith('http://www.corteidh.or.cr/docs/casos/articulos/seriec_') &&
+            anchor.href.endsWith('_esp.pdf')
+            ) {
+          anchor.setAttribute("download", filename);
+          anchor.click();
+          return filename;
+        }
+      });
+    }, resultsSelector)).filter(x => !!x);
+
+    // Wait for downloads to finish an arbitrary amount of time, I couldn't find
+    // any callback.
+    await page.waitForTimeout(20000);
+
+    await browser.close();
+  } catch (e) {
+    await page.screenshot({
+      path: 'failed.jpg'
+    });
+    throw e;
+  }
+})();
