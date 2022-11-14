b7c6e1974855120c6ed4ec2e57e5d9ebed1ec939
Sebastian Waisbrot <seppo0010@gmail.com>
2022-11-14 12:27:06 -0300

    Use gzip -n to make it (more?) deterministic

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ node 0-scraping/index.js
 bash 1-extract/convertall.sh
 python3 2-structure/build.py
 node 3-index/index.js
-gzip $DATA_DIR/data.json --stdout > 4-web/public/data.json.gz
+gzip -n $DATA_DIR/data.json --stdout > 4-web/public/data.json.gz
 pushd 4-web
 npm run deploy
